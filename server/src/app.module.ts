ef1226a2a02959ba81efd8247f4ce829ce280e3e
lpoulter1 <thundercatontheloose@gmail.com>
2023-06-13 16:44:52 +0100

    fix server paths

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PlayersModule } from './players/players.module';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [PlayersModule, TodosModule],
+  imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
