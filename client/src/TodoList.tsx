51cf487c2c99583258c39aa5aef95a77d0983840
lpoulter1 <thundercatontheloose@gmail.com>
2023-06-13 16:23:01 +0100

    css

--- client/src/TodoList.tsx
+++ client/src/TodoList.tsx
@@ -8,12 +8,14 @@ export function TodoList({ todos }: { todos: Todo[] }) {
   return (
     <div>
       <h1>Todo List</h1>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <h2>{todo.title}</h2>
-          <p>{todo.description}</p>
-        </div>
-      ))}
+      <div className="flex flex-col gap-4">
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <h2>{todo.title}</h2>
+            <p>{todo.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
